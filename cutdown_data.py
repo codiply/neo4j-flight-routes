b2a1e37f548dd3cc25d41f964ff595e47a429d66
Panos <codiply@gmail.com>
2015-02-01 12:18:20 +0000

    Eliminated long lines.

--- cutdown_data.py
+++ cutdown_data.py
@@ -19,15 +19,22 @@ def main():
 
     country = 'United Kingdom'
     
-    airline_fieldnames = ['id','name','alias','iata','icao','callsign','country','active']
+    airline_fieldnames = ['id','name','alias','iata',
+                          'icao','callsign','country','active']
     airline_pred = lambda row: row['country'] == country and row['active'] == 'Y'
-    airline_ids = cutdown('data/airlines.dat', 'data/airlines_cutdown.dat', airline_fieldnames, airline_pred, 'id')
+    airline_ids = cutdown('data/airlines.dat', 'data/airlines_cutdown.dat', 
+                          airline_fieldnames, airline_pred, 'id')
 
-    airport_fieldnames = ['id','name','city','country','iata_faa','icao','latitude','longitude','altitude','timezone','dst', 'tz_timezone']
+    airport_fieldnames = ['id','name','city','country','iata_faa',
+                          'icao','latitude','longitude','altitude',
+                          'timezone','dst', 'tz_timezone']
     airport_pred = lambda row: row['country'] == country
-    airport_ids = cutdown('data/airports.dat', 'data/airports_cutdown.dat', airport_fieldnames, airport_pred, 'id')  
+    airport_ids = cutdown('data/airports.dat', 'data/airports_cutdown.dat', 
+                          airport_fieldnames, airport_pred, 'id')  
 
-    route_fieldnames = ['airline','airline_id','source_airport','source_airport_id','destination_airport','destination_airport_id', 'codeshare','stops','equipment']
+    route_fieldnames = ['airline','airline_id','source_airport','source_airport_id',
+                        'destination_airport','destination_airport_id', 
+                        'codeshare','stops','equipment']
     route_pred = lambda row: (row['airline_id'] in airline_ids and 
                               row['source_airport_id'] in airport_ids and 
                               row['destination_airport_id'] in airport_ids)
